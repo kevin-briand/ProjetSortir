96071f620a8feece94bd4c8531973dcc5d235fcf
Aurore Dubray <a.dubray64@gmail.com>
2023-02-06 17:57:00 +0100

    Design base site

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,10 +8,10 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/main', name: 'app_main')]
-    public function index(): Response
+    #[Route('/', name: 'app_main')]
+    public function home(): Response
     {
-        return $this->render('main/index.html.twig', [
+        return $this->render('main/home.html.twig', [
             'controller_name' => 'MainController',
         ]);
     }
